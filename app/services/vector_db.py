d226d58c58bdd85241743de3f8d8327158a5dbf5
taganliu <taganliu@cvut.cz>
2026-08-04 16:50:07 +0200

    Creating embeddings and saving them to pgvector

---
+++ app/services/vector_db.py
@@ -0,0 +1,43 @@
+from app.database import get_connection
+
+
+def save_document(
+    filename,
+    page,
+    chunk_id,
+    content,
+    embedding
+):
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+
+    cursor.execute(
+        """
+        INSERT INTO documents
+        (
+            filename,
+            page,
+            chunk_id,
+            content,
+            embedding
+        )
+        VALUES
+        (%s,%s,%s,%s,%s)
+        """,
+        (
+            filename,
+            page,
+            chunk_id,
+            content,
+            embedding
+        )
+    )
+
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
